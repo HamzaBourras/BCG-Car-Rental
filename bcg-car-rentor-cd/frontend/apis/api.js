042e32ad1b3c86715dbe1e416d61c48447b6aeda
HamzaBourras <bourrashamza2004@gmail.com>
2025-04-16 19:35:49 +0000

    fonction inscrire js

--- bcg-car-rentor-cd/frontend/apis/api.js
+++ bcg-car-rentor-cd/frontend/apis/api.js
@@ -2,6 +2,9 @@ export const baseUrl = "http://localhost:8000/";
 
 /******* Authentification *******/
 export const INSCRIRE_API = `${baseUrl}api/auth/inscrire`;
+export const CONNEXION_API = `${baseUrl}api/auth/connecter`;
+export const DECONNEXION_API = `${baseUrl}api/auth/deconnecter`;
+export const MODIFIER_PROFILE_API = `${baseUrl}api/auth/modifierProfile`;
 
 /******* api public *******/
 export const INDEX_VOITURES = `${baseUrl}api/public/voitures/index`;
--- bcg-car-rentor-cd/frontend/js/functions/sendData.js
+++ bcg-car-rentor-cd/frontend/js/functions/sendData.js
@@ -1,7 +1,6 @@
 const sendData = {
   returnData: null,
   token: null,
-  reponse: null,
   success: null,
   message: null,
   errors: null,
@@ -13,6 +12,9 @@ const sendData = {
     this.resetChamps();
 
     try {
+      const config = {
+        headers: {}
+      }
       // s'il y a un fichier
       if (hasFile) {
         config.headers["Content-Type"] = "multipart/form-data";
@@ -21,17 +23,30 @@ const sendData = {
       }
 
       // envoi de la requete
-      const response = await axios[method](apiUrl, formData, config);
+      // const response = await axios[method](apiUrl, formData, config);
+      const response = await axios({
+        method,
+        url: apiUrl,
+        data: formData,
+        headers: config.headers
+      });
+
 
       // Traitement réponse réussie
-      this.reponse = response;
       this.message = response.data?.message;
       this.returnData = response.data?.data;
       this.success = response.data?.success;
     } catch (error) {
-      this.success = error.response?.data?.success || false;
-      this.errors = error.response?.data?.errors || "Erreur lors de la requête";
-      throw error;
+      // s'il y a des erreurs de validation
+      if (error.response?.status === 422) {
+        this.errors = error.response.data?.errors;
+        this.message = "Erreurs de validation";
+      } else {
+        this.message = error.response?.data?.message;
+        this.errors = error.response?.data?.errors;
+      }
+      this.success = error.response?.data?.success;
+      throw error; // Important pour que le catch dans inscrire.js soit déclenché
     }
   },
 
@@ -39,10 +54,9 @@ const sendData = {
     this.errors = null;
     this.message = null;
     this.returnData = null;
-    this.response = null;
     this.token = null;
     this.success = null;
   },
 };
 
-export default apiHandler;
+export default sendData;
